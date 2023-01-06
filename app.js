75a500628b1925d43f7531d9c1529604d2eeaa32
sami1793 <sami_17_94@hotmail.com>
2023-01-06 17:45:00 -0300

    Se programa funcionamiento de menu burguer en archivo JS

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 const $ = (selector) => document.querySelector(selector);
+
+// Funcionamiento menú burguer
+$('#navbar-burguer').addEventListener('click', () =>{
+    $('#navbar-burguer').classList.toggle("is-active")
+    $('#navbar').classList.toggle("is-active")
+})
