9789a5a7d662d5143b8c21eb242dec3886f220e2
sami1793 <sami_17_94@hotmail.com>
2023-01-06 20:07:27 -0300

    Se agrega array con objetos categorias, en un container se van agregando todas estas categorias mediante un ciclo for

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ $('#navbar-burguer').addEventListener('click', () =>{
     $('#navbar-burguer').classList.toggle("is-active")
     $('#navbar').classList.toggle("is-active")
 })
+// Se muestran las categorias del array categorias
+const generateCategories = (categories) =>{
+    for(let {name} of categories){
+        $("#container-categories").innerHTML +=
+        `<div class="level is-mobile">
+        <span class="level-left tag is-primary is-light">${name}</span>
+        <span class="level-right">
+            <a class="level-item is-size-7">Editar</a>
+            <a class="level-item is-size-7">Eliminar</a>
+        </span>
+    </div>`
+    }
+}
+generateCategories(categories);
---
+++ categorias.js
@@ -0,0 +1,20 @@
+const categories = [
+    {
+        name: "Comida"
+    },
+    {
+        name: "Servicios"
+    },
+    {
+        name: "Salidas"
+    },
+    {
+        name: "Educación"
+    },
+    {
+        name: "Transporte"
+    },
+    {
+        name: "Trabajo"
+    }
+]
