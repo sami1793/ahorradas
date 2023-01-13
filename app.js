c9fb29012dae20397904079448a9747c41b16947
sami1793 <sami_17_94@hotmail.com>
2023-01-13 16:41:20 -0300

    Se corrigen bugs de edicion

--- app.js
+++ app.js
@@ -2,29 +2,33 @@ const $ = (selector) => document.querySelector(selector);
 
 //Funcionamiento nav balance/categorias/reportes
 
-let goToBalance = () =>{
+let goToBalance = () => {
   $("#view-balance").classList.remove("oculto");
   $("#view-categories").classList.add("oculto");
   $("#view-reports").classList.add("oculto");
 
   $("#view-balance-operation-filter").classList.remove("oculto");
   $("#view-new-operation").classList.add("oculto");
-} 
+};
 
-$("#balance-link").addEventListener("click", goToBalance);
-
-$("#categories-link").addEventListener("click", () => {
+let goToCategories = () => {
   $("#view-balance").classList.add("oculto");
   $("#view-categories").classList.remove("oculto");
   $("#view-reports").classList.add("oculto");
+
   $("#view-edit-category").classList.add("oculto");
   $("#view-category").classList.remove("oculto");
-});
-$("#reports-link").addEventListener("click", () => {
+};
+
+let goToReports = () => {
   $("#view-balance").classList.add("oculto");
   $("#view-categories").classList.add("oculto");
   $("#view-reports").classList.remove("oculto");
-});
+};
+
+$("#balance-link").addEventListener("click", goToBalance);
+$("#categories-link").addEventListener("click", goToCategories);
+$("#reports-link").addEventListener("click", goToReports);
 
 // Funcionamiento menú burguer
 $("#navbar-burguer").addEventListener("click", () => {
@@ -45,7 +49,7 @@ $("#link-hide-filters").addEventListener("click", () => {
 // Mostrar las categorias del array
 const generateCategories = (categories) => {
   $("#container-categories").innerHTML = ""; //refresco el array
-  $('#category-new-operation-input').innerHTML='';//refresco el desplegable categories
+  $("#category-new-operation-input").innerHTML = ""; //refresco el desplegable categories
   for (let { name, id } of categories) {
     $("#container-categories").innerHTML += `<div class="level is-mobile">
         <span class="level-left tag is-primary is-light">${name}</span>
@@ -55,8 +59,7 @@ const generateCategories = (categories) => {
         </span>
     </div>`;
     //Actualizo Desplegable Categorias
-    $('#category-new-operation-input').innerHTML += 
-    `<option>${name}</option>`
+    $("#category-new-operation-input").innerHTML += `<option>${name}</option>`;
   }
 };
 generateCategories(categories);
@@ -93,30 +96,52 @@ let openEditCategory = (id) => {
 };
 
 let editCategory = (id) => {
-  $("#edit-category-button").addEventListener("click", () => {
-    let newCategory = $("#new-category-input").value;
+  let isClosedEditCategory = false;
 
-    categories.map((c) => {
-      if (c.id === id) {
-        c.name = newCategory;
-      }
-    });
-
-    // vuelvo a la vista anterior
-    generateCategories(categories);
-    $("#view-edit-category").classList.add("oculto");
-    $("#view-category").classList.remove("oculto");
+  $("#cancel-edit-category-button").addEventListener("click", () => {
+    isClosedEditCategory = true;
+    cancelEditCategory();
   });
+
+  //Prevengo por si hace click en el nav
+  $("#balance-link").addEventListener("click", () => {
+    isClosedEditCategory = true;
+  });
+  $("#categories-link").addEventListener("click", () => {
+    isClosedEditCategory = true;
+  });
+  $("#reports-link").addEventListener("click", () => {
+    isClosedEditCategory = true;
+  });
+
+  $("#edit-category-button").addEventListener("click", functionEditCategory, {
+    once: true, //este atributo hace que solo se ejecute una vez
+  });
+
+  function functionEditCategory(e) {
+    if (!isClosedEditCategory) {
+      let newCategory = $("#new-category-input").value;
+
+      categories.map((c) => {
+        if (c.id === id) {
+          c.name = newCategory;
+        }
+      });
+    }
+
+    // vuelvo a la vista principal de Categorias
+    generateCategories(categories);
+    goToCategories();
+  }
 };
 
 //Cancelar editar categoria
 let cancelEditCategory = () => {
   $("#cancel-edit-category-button").addEventListener("click", () => {
-    $("#view-edit-category").classList.add("oculto");
-    $("#view-category").classList.remove("oculto");
+    goToCategories();
+    return true;
   });
 };
-
 
 // **************************
 // ********OPERACIONES*******
@@ -124,39 +149,89 @@ let cancelEditCategory = () => {
 let operations = [];
 
 //Agregar operacion
-let addOperation = () =>{
-  let descriptionInfo = $('#description-new-operation-input').value
-  let amountInfo = $('#amount-new-operation-input').value
-  let typeInfo = $('#type-new-operation-input').value
-  let categoryInfo = $('#category-new-operation-input').value
-  let dateInfo = $('#date-new-operation-input').value
+let addOperation = () => {
+  let descriptionInfo = $("#description-new-operation-input").value;
+  let amountInfo = $("#amount-new-operation-input").value;
+  let typeInfo = $("#type-new-operation-input").value;
+  let categoryInfo = $("#category-new-operation-input").value;
+  let dateInfo = $("#date-new-operation-input").value;
 
   let sizeOperations = operations.length;
   let lastIdOperations;
-  if(sizeOperations>=1)
-    lastIdOperations = operations[sizeOperations - 1].id;
-  else 
-    lastIdOperations = 0;
+  if (sizeOperations >= 1) lastIdOperations = operations[sizeOperations - 1].id;
+  else lastIdOperations = 0;
 
   operations.push({
     id: lastIdOperations + 1,
     description: descriptionInfo,
     amount: amountInfo,
-    type : typeInfo,
-    category : categoryInfo,
-    date : dateInfo
-  })
-}
-let cleanInputOperation = () =>{
-  $('#description-new-operation-input').value ='';
-  $('#amount-new-operation-input').value ='';
-  $('#type-new-operation-input').value ='';
-  $('#category-new-operation-input').value ='';
-  $('#date-new-operation-input').value ='';
-}
-$('#add-new-operation-button').addEventListener('click', ()=>{
-  addOperation();
-  cleanInputOperation();
-  goToBalance();
-})
+    type: typeInfo,
+    category: categoryInfo,
+    date: dateInfo,
+  });
+};
 
+let cleanInputOperation = () => {
+  $("#description-new-operation-input").value = "";
+  $("#amount-new-operation-input").value = "";
+  $("#type-new-operation-input").value = "";
+  $("#category-new-operation-input").value = "";
+  $("#date-new-operation-input").value = "";
+};
+
+// Mostrar Operaciones
+let showOperations = () => {
+  $("#with-operations-container").innerHTML = " ";
+  for (let { id, description, amount, type, category, date } of operations) {
+    $(
+      "#with-operations-container"
+    ).innerHTML += `<div class="columns is-centered has-text-weight-semibold is-hidden-mobile">
+    <div class="column is-3">
+        <span>Descripcion</span>
+    </div>
+    <div class="column is-3">
+        <span>Categoría</span>
+    </div>
+    <div class="column is-2">
+        <span>Fecha</span>
+    </div>
+    <div class="column is-2">
+        <span>Monto</span>
+    </div>
+    <div class="column is-2">
+        <span>Acciones</span>
+    </div>
+</div>
+<div class="columns is-multiline is-mobile is-vcentered">
+    <div class="column is-3-tablet is-6-mobile">
+        <span class="has-text-weight-semibold">
+        ${description}
+        </span>
+    </div>
+    <div class="column is-3-tablet is-6-mobile has-text-right-mobile">
+        <span class="tag is-primary is-light">${category}</span>
+    </div>
+    <div class="column is-2-tablet is-hidden-mobile">
+        <span>${date}</span>
+    </div>
+    <div class="column is-2-tablet is-6-mobile">
+        <span>${amount}</span>
+    </div>
+    <div class="column is-2-tablet is-6-mobile has-text-right-mobile">
+            <p class="is-fullwidth">
+                <a class=" is-size-7 mr-2 ">Editar</a>
+                <a class=" is-size-7">Eliminar</a>
+            </p>
+    </div>
+</div>`;
+  }
+};
+
+let addNewOperation = () => {
+  $("#add-new-operation-button").addEventListener("click", () => {
+    addOperation();
+    cleanInputOperation();
+    showOperations();
+    goToBalance();
+  });
+};
