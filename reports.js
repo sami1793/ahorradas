3294223b67bf915551a7edef7e0f5deb602e5580
sami1793 <sami_17_94@hotmail.com>
2023-01-17 11:56:10 -0300

    Se crean funciones que obtienen totales por categorias

---
+++ reports.js
@@ -0,0 +1,49 @@
+//Obtener Totales por Categorias
+let totalsOperations = (operations) =>{
+    let totalGanancias = 0;
+    let totalGastos = 0;
+    let balance = 0;
+
+    for (let { id, description, amount, type, category, date } of operations){
+        if (type === "Gasto") {
+            totalGastos += Number(amount);
+        } 
+        else if (type == "Ganancia") {
+            totalGanancias += Number(amount);
+        }
+
+        balance = totalGanancias - totalGastos;
+    }
+
+    return {
+        totalGanancias : totalGanancias,
+        totalGastos : totalGastos,
+        balance :  balance
+    }
+}
+
+let getTotalsCategories = (operations)=>{
+    let totalGastosCategory = 0;
+    let totalGananciasCategory = 0;
+    let balanceCategory= 0;
+    let totalsCategories = [];
+    let copyOperations = [...operations];
+
+    for (let category of categories) {
+        total = totalsOperations(filterCategory(copyOperations, category.name));
+        totalGastosCategory = total.totalGastos;
+        totalGananciasCategory =  total.totalGanancias;
+        balanceCategory = total.balance;
+
+        totalsCategories.push({
+            category : category.name,
+            totalGanancias: totalGananciasCategory,
+            totalGastos: totalGastosCategory,
+            balance: balanceCategory
+        })
+    }
+
+    return totalsCategories;
+}
+
+//Mostrar totales por categorias
